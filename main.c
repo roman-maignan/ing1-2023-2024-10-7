9308d3ae4fe20c096c009ff3bc757f850ccdfdc1
roman-maignan <149956820+roman-maignan@users.noreply.github.com>
2023-12-03 23:33:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include "header.h"
 
 int main() {
-    printf("salut roman!\n");
+    t_temps t;
+    affm();
+    affs();
+    affo();
+    afft();
+    char touche;
+    t.t1=60;
+    while (touche!='p') {
+        gotoligcol(12,9);
+        printf("%d",t.t1);
+        t.t1=t.t1-1;
+        delay(1000);
+    }
     return 0;
 }
